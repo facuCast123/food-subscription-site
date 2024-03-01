0d2aaa3813e62beb0040df98053f8e4152e31238
facuCast123 <facundo022020@gmail.com>
2024-03-01 20:57:10 -0300

    Implemented animated showcase

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,24 @@
+import { useEffect, useRef } from "react";
+
+import Meal from "../components/landing-page/Meal";
+
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+
 const LandingPage = () => {
+  const showcaseInner = useRef(null);
+
+  useEffect(() => {
+    const showcaseContent = Array.from(showcaseInner.current.children);
+
+    showcaseContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      showcaseInner.current.appendChild(duplicatedItem);
+    });
+  }, []);
+
   return (
     <main>
       <div className="main__container">
@@ -38,9 +58,34 @@ const LandingPage = () => {
       <div className="meals__slider">
         <h1>Healthy & Tasty</h1>
         <p>
-          You can mix and as you like depending on the occasion and mood. Over
-          40 original recipes.
+          You can mix and match as you like depending on the occasion and mood.
+          Over 40 original recipes.
         </p>
+
+        <div className="meals__slider__select">
+          <p>Main dish</p>
+          <p>Soup</p>
+          <p>Salad</p>
+          <p>Smoothie</p>
+          <p>Set Menu</p>
+        </div>
+
+        <div className="meals__slider__wrapper">
+          <Meal />
+          <Meal />
+          <Meal />
+        </div>
+      </div>
+
+      <div className="showcase" data-animated="true">
+        <div className="showcase__inner" ref={showcaseInner}>
+          <img src="./images/foods/food-1.jpg" />
+          <img src="./images/foods/food-2.jpg" />
+          <img src="./images/foods/food-3.jpg" />
+          <img src="./images/foods/food-4.jpg" />
+          <img src="./images/foods/food-5.jpg" />
+          <img src="./images/foods/food-6.jpg" />
+        </div>
       </div>
     </main>
   );
